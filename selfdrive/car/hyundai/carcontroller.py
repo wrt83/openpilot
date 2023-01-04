0dcda6539c19e265dc5307f6702b585b9c7fa11c
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2023-01-04 12:10:51 -0800

    ev6 more torque + blinkers

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -59,8 +59,11 @@ class CarController:
     hud_control = CC.hudControl
 
     # steering torque
-    new_steer = int(round(actuators.steer * self.params.STEER_MAX))
+    steer = actuators.steer
+    self.params = CarControllerParams(self.CP, CS.out.vEgoRaw)
+    new_steer = int(round(steer * self.params.STEER_MAX))
     apply_steer = apply_std_steer_torque_limits(new_steer, self.apply_steer_last, CS.out.steeringTorque, self.params)
+    apply_steer = clip(apply_steer, -self.params.STEER_MAX, self.params.STEER_MAX)
 
     if not CC.latActive:
       apply_steer = 0
